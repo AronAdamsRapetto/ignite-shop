1cf9990ee1a1bdd754a8d603b25628b057dd28dc
Aron Adams <aronrapetto@hotmail.com>
2023-05-29 16:33:35 -0300

    feat: modulo 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,70 @@
-import { styled } from "@/styles"
+import Image from "next/image"
+import { GetStaticProps } from "next"
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 8,
-  border: 0,
-  padding: '4px 8px',
+import { stripe } from "@/lib/stripe"
+import { HomeContainer, Product } from "@/styles/pages/home"
 
-  span: {
-    fontWeight: 'bold'
-  },
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
+import Stripe from "stripe"
 
-  '&:hover': {
-    filter: 'brightness(0.8)' 
-  }
-})
+interface HomeProps {
+  products: {
+    id: string
+    name: string,
+    imageUrl: string
+    price: number
+  }[]
+}
 
-export default function Home() {
+export default function Home({ products }: HomeProps) {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-   <Button>
-    <span>Teste</span>
-    Hello world</Button>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      {products.map((product) => (
+        <Product key={product.id} className="keen-slider__slide">
+          <Image src={product.imageUrl} width={520} height={480} alt="" />
+
+          <footer>
+            <strong>{product.name}</strong>
+            <span>{product.price}</span>
+          </footer>    
+        </Product>
+      ))}
+    </HomeContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  })
+
+  const products = response.data.map(product => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format(price.unit_amount as number / 100)
+      
+    }
+  })
+
+  return { 
+    props: {
+      products
+    },
+    revalidate: 60 * 60 * 2 // 2 hours 
+  }
 }
